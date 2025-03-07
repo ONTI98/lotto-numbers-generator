edd85e7a48f516167058284e67896f40fc4a2f9d
ONTI98 <157315560+ONTI98@users.noreply.github.com>
2025-03-07 17:03:17 +0200

    second commit

--- lotto numbers.py
+++ lotto numbers.py
@@ -1,32 +1,49 @@
-#generate random numbers 
-
+#generate random numbers
+  
 import random
 from time import *
 
+def main ():
+    generate_powerball_number(list_name=powerball_number)
+     
+    generate_numbers(name_of_list=first_five)
+
+    print()
+
+powerball=[]
 numbers=[]
+previous_number=None
 
-list1=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,
-      33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52]
+first_five=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,
+      33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50]
 
-list2=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,
-      33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62]
+powerball_number=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
-def generate_number(name_of_list):
+def generate_numbers(name_of_list):
+    counter= 0
     while True:
 
-        ask=input("would you like to generate number (Y/N)?")
+        while counter <= 4:
+                
+                random_number=random.choices(first_five)
+                numbers.append(random_number)
 
-        if ask.upper() == "Y":
-            generate_number=random.choice(name_of_list)
-            numbers.append(generate_number)
-            print(numbers)
-        else:
-            sleep(3)
-            print(f"Your lucky numbers are {numbers}")
-            print("GOOD LUCK!!!")
-            break 
+                counter +=1
 
-generate_number(name_of_list=list1)
+                if counter == 5:
+                #remove certain elements from list
+                    numbers2=[i[0] for i in numbers ]
+                    print(f"Lucky numbers: {numbers2}")
 
+                else:
+                     break
+
+def generate_powerball_number(list_name):
+        
+    generate_powerball_number=random.choice(list_name)       
+    powerball.append(generate_powerball_number)
+    print(f"Powerball: {powerball}")
+
+main ()
 
 
